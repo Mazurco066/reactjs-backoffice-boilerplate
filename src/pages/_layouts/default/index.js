e64371ffcd6a04bc25a1e839a2f22331d17f0d6e
Gabriel Ribeiro <mazurco066@gmail.com>
2021-05-22 16:31:12 -0300

    feat(drawer): added drawer for mobile

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -5,8 +5,8 @@ import { SidebarItems } from 'utils'
 import { useHistory } from 'react-router-dom'
 
 // Components
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
-import { Layout, Menu } from 'antd'
+import { CloseOutlined, MenuOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
+import { Drawer, Layout, Menu } from 'antd'
 
 // Destruct Antd
 const { Header, Sider, Content } = Layout
@@ -18,9 +18,42 @@ export default function DefaultLayout({ children }) {
   // Hooks
   const router = useHistory()
   const [ collapsed, setCollapsed ] = useState(false)
+  const [ isDrawerOpen, setDrawerOpenState ] = useState(false)
+
+  // Menu Renderer
+  const renderMenu = () => (
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+      { 
+        SidebarItems.map(({ text, icon, children, key, path }) => children.length > 0 ? (
+          <SubMenu key={key} icon={icon} title={text}>
+            {
+              children.map(({ text: subTitle, icon: subIcon, key: subKey, path: subPath }) => (
+                <Item
+                  onClick={() => router.push(subPath)}
+                  key={subKey}
+                  icon={subIcon ? subIcon : null}
+                >
+                  {subTitle}
+                </Item>
+              ))
+            }
+          </SubMenu>
+        ) : (
+          <Item
+            key={key}
+            icon={icon ? icon : null}
+            onClick={() => router.push(path)}
+          >
+            {text}
+          </Item>
+        ))
+      }
+    </Menu>
+  )
 
   // Actions
   const toggleSidebar = () => setCollapsed(!collapsed)
+  const toggleDrawer = () => setDrawerOpenState(!isDrawerOpen)
   
   // JSX
   return (
@@ -31,39 +64,28 @@ export default function DefaultLayout({ children }) {
         collapsible
       >
         <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          { 
-            SidebarItems.map(({ text, icon, children, key, path }) => children.length > 0 ? (
-              <SubMenu key={key} icon={icon} title={text}>
-                {
-                  children.map(({ text: subTitle, icon: subIcon, key: subKey, path: subPath }) => (
-                    <Item
-                      onClick={() => router.push(subPath)}
-                      key={subKey}
-                      icon={subIcon ? subIcon : null}
-                    >
-                      {subTitle}
-                    </Item>
-                  ))
-                }
-              </SubMenu>
-            ) : (
-              <Item
-                key={key}
-                icon={icon ? icon : null}
-                onClick={() => router.push(path)}
-              >
-                {text}
-              </Item>
-            ))
-          }
-        </Menu>
+        { renderMenu() }
       </Sider>
+      <Drawer
+        title="Backoffice Boilerplate"
+        placement="right"
+        closable={true}
+        onClose={() => setDrawerOpenState(false)}
+        visible={isDrawerOpen}
+        getContainer={false}
+        style={{ position: 'absolute' }}
+      >
+        { renderMenu() }
+      </Drawer>
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }}>
           {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
             className: 'trigger',
             onClick: toggleSidebar,
+          })}
+          {createElement(isDrawerOpen ? CloseOutlined : MenuOutlined, {
+            className: 'trigger-mobile',
+            onClick: toggleDrawer,
           })}
         </Header>
         <Content
