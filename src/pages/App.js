f54db718be964f72c9b7d90a59a2be1403d3e5cc
Gabriel Ribeiro <mazurco066@gmail.com>
2021-05-22 12:01:33 -0300

    feat(project): init

---
+++ src/pages/App.js
@@ -0,0 +1,16 @@
+// Dependencies
+import { ToastContainer } from 'react-toastify'
+
+// Components
+import { Button } from 'antd'
+
+// Component
+export default function App() {
+  return (
+    <div className="App">
+      <p>Teste</p>
+      <Button type="primary">Button</Button>
+      <ToastContainer position="bottom-right" />
+    </div>
+  )
+}
